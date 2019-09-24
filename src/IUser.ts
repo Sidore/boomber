703be8402fa9d2b8cd98da827c9d1930e6493de3
Vladyslav Sydorenko <Vladyslav_Sydorenko@epam.com>
2019-09-24 17:32:59 +0300

    intaraction

---
+++ src/IUser.ts
@@ -0,0 +1,6 @@
+export default interface IUser {
+    name: string;
+    nick: string;
+    skills: string[];
+    exp: number;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as path from "path";
 import * as socketServer from "socket.io";
+import IUser from "./IUser";
 
 
 const server = express();
@@ -14,22 +15,51 @@ const httpServer = server.listen(3333, () => {
     console.log("run on port 3333")
 })
 
-const userCollection = {}
+interface IUserCollection {
+    [key: string] : IUser;
+}
+
+const userCollection: IUserCollection  = {}
 
 const io = socketServer(httpServer);
 io.on("connection", (socket) => {
 
-    userCollection[socket.id] = {}
+    userCollection[socket.id] = {
+        name: "",
+        nick: "",
+        skills: [],
+        exp: 0
+    }
 
-    console.log('a user connected');
+    console.log('a new user connected');
 
-    socket.on("chat message", (data) => {
-        userCollection[socket.id].name = data;
+    socket.on("set data", (data) => {
+        if (data && data.name) {
+
+            for (let a in userCollection) {
+                // console.log(a, userCollection[a])
+                
+                if (userCollection[a].name) {
+                    console.log(userCollection[a].name, "should be displayed to", data.name)
+                    socket.emit("new player", userCollection[a].name);
+                }
+            }
+
+            userCollection[socket.id].name = data.name;
+
+            
+            io.emit("new player", userCollection[socket.id].name)
+        }
+
         console.log(data);
     })
 
     socket.on("direction", (data) => {
-        console.log(userCollection[socket.id].name,data);
+        console.log(userCollection[socket.id].name, data);
+        io.emit("move block", {
+            player : userCollection[socket.id].name,
+            move : data
+        })
     })
 
     
